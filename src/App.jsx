6d39c96098b8536204f2024e68afd032e763d49b
Alduindev <urielcabadat@hotmail.com>
2026-07-01 10:28:28 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
-import AuthState from "./context/auth/authState";
 
 export default function App() {
   return (
     <BrowserRouter basename="/portfolio">
-      <AuthState>
         <Portfolio />
-      </AuthState>
     </BrowserRouter>
   );
 }
